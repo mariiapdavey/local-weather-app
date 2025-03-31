b9e5a9bc9564d111cbe9c0269c0fe91920a02181
mariiapdavey <mariiapdavey@gmail.com>
2025-03-30 23:33:45 -0700

    added app.module.ts; current weather component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CurrentWeatherComponent } from "./current-weather/current-weather.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CurrentWeatherComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'local-weather-app';
--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ICurrentWeather } from '../icurrent-weather';
 
 @Component({
   selector: 'app-current-weather',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './current-weather.component.css'
 })
 export class CurrentWeatherComponent {
-
+  current: ICurrentWeather = {
+    city: 'Seattle',
+    country: 'US',
+    date: new Date(),
+    image: '',
+    temperature: 67,
+    description: 'Sunny weather'
+  }
 }
