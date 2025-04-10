40aa2948b3fb36996e6e08b5b26d9a0913df44dd
mariiapdavey <mariiapdavey@gmail.com>
2025-04-10 10:17:47 -0700

    styling adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [CurrentWeatherComponent]
+  imports: [CurrentWeatherComponent, 
+            MatToolbarModule,
+            MatCardModule,
+            FontAwesomeModule
+            ]
 })
 export class AppComponent {
   title = 'local-weather-app';
--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { ICurrentWeather } from '../icurrent-weather';
 import { CommonModule, DatePipe, DecimalPipe } from '@angular/common';
 import { WeatherService } from '../weather.service';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {faCoffee} from '@fortawesome/free-solid-svg-icons'
+
 
 @Component({
   selector: 'app-current-weather',
   standalone: true,
-  imports: [DatePipe, DecimalPipe, CommonModule],
+  imports: [DatePipe, 
+            DecimalPipe, 
+            CommonModule, 
+            MatToolbarModule, 
+            MatCardModule],
   templateUrl: './current-weather.component.html',
   styleUrl: './current-weather.component.css'
 })
 export class CurrentWeatherComponent {
+  faCoffee=faCoffee;
   current: ICurrentWeather = {
     city: '',
     country: '',
